7e2cf4310e8a4477c6eff731a7c0a70df523f214
lordvaynard <ronieseixas@gmail.com>
2022-11-11 21:08:46 -0300

    variaveis de ambiente

--- src/components/RegisterVideo/index.js
+++ src/components/RegisterVideo/index.js
@@ -23,8 +23,11 @@ function useForm(propsDoForm) {
     }
 }
 
- const PROJECT_URL = process.env.PROJECT_URL
- const PUBLIC_KEY = process.env.PUBLIC_KEY
+const PROJECT_URL = "https://ooizylnrmiyifscnuwiu.supabase.co";
+const PUBLIC_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9vaXp5bG5ybWl5aWZzY251d2l1Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY2ODE5MjI2MCwiZXhwIjoxOTgzNzY4MjYwfQ.opEE_c0cm4HWxmElvoPLwcYx-_Vf0mALUGIbEDZTBzI";
+
+// const NEXT_PUBLIC_PROJECT_URL = process.env.PROJECT_URL
+// const NEXT_PUBLIC_KEY = process.env.PUBLIC_KEY
 const supabase = createClient(PROJECT_URL, PUBLIC_KEY)
 
 // get youtube video id
--- src/services/videoService.js
+++ src/services/videoService.js
@@ -1,7 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
-const _PROJECT_URL = process.env.PROJECT_URL
-const _PUBLIC_KEY =  process.env.PUBLIC_KEY
+const _PROJECT_URL = "https://ooizylnrmiyifscnuwiu.supabase.co";
+const _PUBLIC_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9vaXp5bG5ybWl5aWZzY251d2l1Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY2ODE5MjI2MCwiZXhwIjoxOTgzNzY4MjYwfQ.opEE_c0cm4HWxmElvoPLwcYx-_Vf0mALUGIbEDZTBzI";
+// const _PROJECT_URL ==  process.env.PROJECT_URL
+// const _PUBLIC_KEY ==  process.env.PUBLIC_KEY
 const supabase = createClient(_PROJECT_URL, _PUBLIC_KEY)
 
 export function videoService() {
